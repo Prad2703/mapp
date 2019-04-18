bae63c5f17e7e03223514f2d29514b1d98ce28bf
Subhadeep Sheet <subhadeepsheet@infoway.us>
2019-04-18 21:21:06 +0530

    Add Header and footer oncheckout page

---
+++ app/design/frontend/S4p/inovative/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright � 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/S4p/inovative',
+    __DIR__
+);
